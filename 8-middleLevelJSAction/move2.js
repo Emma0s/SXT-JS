0ce47223ee16fb90979902fb4b7ecc4af7b9402b
emma0s <joy0heaven@sina.cn>
2020-03-29 19:32:57 +0800

    solve the forin problem and add ppt exercise

--- 8-middleLevelJSAction/move2.js
+++ 8-middleLevelJSAction/move2.js
@@ -5,11 +5,13 @@ function getStyle(obj, name) {
         return getComputedStyle(obj,false)[name];
     }
 }
+//startMove(oDiv, {width: 400, height: 400})
 
 function startMove(obj, json,fnEnd) {
     clearInterval(obj.timer);
     obj.timer = setInterval(function () {
-        for (var attr in json){
+        var bStop = true;
+        for (var attr in json){  //循环json的所有表头（1.width+speed 2.height+speed）
             var cur = 0;
             if (attr=='opacity'){
                 cur = Math.round(parseFloat(getStyle(obj,attr))*100);
@@ -18,18 +20,19 @@ function startMove(obj, json,fnEnd) {
             }
             var speed = (json[attr]-cur)/6;
             speed = speed>0?Math.ceil(speed):Math.floor(speed);
-            if (cur == json[attr]){
-                clearInterval(obj.timer);
-                if (fnEnd) fnEnd();
-            }else{
-                if (attr=='opacity'){
-                    obj.style.filter = 'alpha(opacity:'+(cur+speed)+')';
-                    obj.style.opacity = (cur+speed)/100;
 
-                }else {
-                    obj.style[attr] = cur+speed+'px';
-                }
+            if (cur != json[attr])  bStop = false;
+
+            if (attr=='opacity'){
+                obj.style.filter = 'alpha(opacity:'+(cur+speed)+')';
+                obj.style.opacity = (cur+speed)/100;
+            }else {
+                obj.style[attr] = cur+speed+'px';
             }
+        }
+        if (bStop){
+            clearInterval(obj.timer);
+            if (fnEnd) fnEnd();
         }
     },30);
 
